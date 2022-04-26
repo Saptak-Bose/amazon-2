8c992ed36cfabc367aa633047a5d2523751d22ce
Saptak <subratabose11@gmail.com>
2022-04-26 18:45:56 +0530

    c

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <Image />
+        </div>
+      </div>
+
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
