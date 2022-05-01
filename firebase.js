bd622a726a28e7c9bbce8d30a8f1ab6441a14160
Saptak <subratabose11@gmail.com>
2022-05-01 10:30:58 +0530

    c

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaRq7GpV_gnVmY7DWbJ9yMTjW_WDgCUsI",
+  authDomain: "fir-f1670.firebaseapp.com",
+  projectId: "fir-f1670",
+  storageBucket: "fir-f1670.appspot.com",
+  messagingSenderId: "466059417427",
+  appId: "1:466059417427:web:4b65c98e3bc7c789b1b3a6",
+  measurementId: "G-S3DSL8REBS",
+};
